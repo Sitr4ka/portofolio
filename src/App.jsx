3f0cbb4ed8e3d7e68163c7e45022a876097e4961
Sitraka Fifaliana <fifasitrakarkt@gmail.com>
2025-10-02 14:13:19 +0300

    ```[FEAT] Add basic routing with BrowserRouter, Layout component and NotFound page```

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router';
+import NotFound from './pages/NotFound';
+import Layout from './components/Layout';
 
 const App = () => {
   return (
-    <div>
-      Hello App
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Layout/>}/>
+        <Route path='projets' element={<Layout/>}/>
+        <Route path='contact' element={<Layout/>}/>
+        <Route path='*' element={<NotFound />}/>
+      </Routes>
+    </BrowserRouter>
   );
 };
 
