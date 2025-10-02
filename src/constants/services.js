5f14710b454e3334efd0a1c9c5e6890424cede35
Sitraka Fifaliana <fifasitrakarkt@gmail.com>
2025-10-02 13:50:49 +0300

    ```[FEAT] Initialize React + Vite + Tailwind project```

---
+++ src/constants/services.js
@@ -0,0 +1,19 @@
+import { RxCrop, RxDesktop, RxPencil2, RxReader, RxRocket } from "react-icons/rx";
+
+export const ServiceData = [
+    {
+        icon: RxCrop,
+        title: 'Site Vitrine',
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    },
+    {
+        icon: RxDesktop,
+        title: 'Application Web',
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    },
+    {
+        icon: RxRocket,
+        title: 'Application Mobile',
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    },
+]
